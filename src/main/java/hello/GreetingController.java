3448a0ea353075d0b225266e9c79551d2cc765f3
Chris Maki <chris.maki@ripcitysoftware.com>
2018-11-28 21:15:56 -0800

    add more messages

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -19,7 +19,7 @@ public class GreetingController {
 
     @RequestMapping("/hello")
     public Greeting hello() {
-        System.out.printf("Logging message to stdout");
+        System.out.printf("Logging message to stdout, with Jenkins integration to syslog");
         throw new RuntimeException("Wrong greeting!");
     }
 }
